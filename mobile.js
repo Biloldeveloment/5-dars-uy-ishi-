9233d1938388e4052c39452af86ca5fada168d9f
Bilol <biloldalimov98@gmail.com>
2022-11-26 12:46:52 +0300

    Initial commit

---
+++ mobile.js
@@ -0,0 +1,10 @@
+for(; ;){
+    let i=+prompt("Son Yozing !");
+    
+if(i%2===0){
+    alert(`${i} soni juft`);
+}else if(i%2===1){
+    alert(`${i} soni toq`);
+}else
+alert("Iltimos 0 dan 10 gacha bo'lgan son yozing!");
+}
